f13c14a4bc14030d7857f56f2d429b92fdb68b58
pixelations <pixelations@users.noreply.github.com>
2016-12-16 12:36:44 +0000

    Add files via upload

--- UBoard.pas
+++ UBoard.pas
@@ -5,11 +5,18 @@ interface
 type
   TBoard = class(TObject)
     private
-
+      rows      : integer;
+      columns   : integer;
+      counters  : integer;
     public
       constructor create(columns, rows : integer);
-
-
+        { initialise variables }
+      function getrows(rows: integer): integer;
+        { "get rows" returns number of rows }
+      function getcolumns(columns: integer): integer;
+        { "get columns" returns number of columns }
+      function getcounters(counters: integer): integer;
+        { "get counters" returns number of counters }
   end;
 
 
