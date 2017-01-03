7f2c2a7b7a6f27d8000fb0274894a696f721da19
pixelations <pixelations@users.noreply.github.com>
2017-01-03 15:43:48 +0000

    Added constants

--- Checkers.dfm
+++ Checkers.dfm
@@ -1,7 +1,7 @@
 object Form1: TForm1
   Left = 0
   Top = 0
-  Caption = 'Form1'
+  Caption = 'Checkers'
   ClientHeight = 281
   ClientWidth = 418
   Color = clBtnFace
--- UBoard.pas
+++ UBoard.pas
@@ -2,6 +2,11 @@ unit UBoard;
 
 interface
 
+const
+  COLUMNS = 8;
+  ROWS = 8;
+  COUNTERS = 24;
+
 type
   TBoard = class(TObject)
     private
@@ -9,7 +14,7 @@ type
       columns   : integer;
       counters  : integer;
     public
-      constructor create(columns, rows : integer);
+      constructor create(columns, rows, counters : integer);
         { initialise variables }
       function getrows(rows: integer): integer;
         { "get rows" returns number of rows }
@@ -21,5 +26,29 @@ type
 
 
 implementation
+
+{ TBoard }
+
+constructor TBoard.create(columns, rows, counters: integer);
+begin
+  columns := COLUMNS;
+  rows := ROWS;
+  counters := COUNTERS;
+end;
+
+function TBoard.getcolumns(columns: integer): integer;
+begin
+  result := columns;
+end;
+
+function TBoard.getcounters(counters: integer): integer;
+begin
+  result := counters;
+end;
+
+function TBoard.getrows(rows: integer): integer;
+begin
+  result := rows;
+end;
 
 end.
