7f4be16d6208d285227b8b068e21d1356ebd72c7
pixelations <pixelations@users.noreply.github.com>
2017-01-31 16:00:46 +0000

    Update

--- USaveLoad.pas
+++ USaveLoad.pas
@@ -2,14 +2,43 @@ unit USaveLoad;
 
 interface
 
+uses
+  UBoard;
+
 type
+  TObjectRecord = Record
+    Counter: TCounter;
+  end;
+
+  ObjectFile = file of TObjectRecord;
+
   TSaveLoad = class(Tobject)
     private
-
+      BoardSave: TObjectRecord;
+      FileName: string;
     public
-
+      constructor Create(AFileName: string);
+      function Save(Board: TObjectArray): boolean;
+      function Load(AFileName: string): TObjectRecord;
   end;
 
 implementation
+
+{ TSaveLoad }
+
+constructor TSaveLoad.Create(AFileName: string);
+begin
+  FileName := AFileName;
+end;
+
+function TSaveLoad.Load(BoardFile): TObjectRecord;
+begin
+
+end;
+
+function TSaveLoad.Save(Board: TObjectArray): boolean;
+begin
+
+end;
 
 end.
